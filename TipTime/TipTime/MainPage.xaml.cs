786c7ce647b86d324c167ff9abf1e17693fdf9ef
marinho777 <danielmarinhom@gmail.com>
2024-03-28 12:36:56 -0300

    chore: first commit

---
+++ TipTime/TipTime/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace TipTime
+{
+    public partial class MainPage : ContentPage
+    {
+        float total = 0;
+        float gorjeta = 0;
+        float valor = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            //valor da refeicao (entry)
+            //gorjeta (label)
+            //total (label)
+            //slider para a % de gorjeta
+            //preset buttons para % de gorjeta
+
+            //arredonda o total com gorjeta pra baixo (button)
+            //arredonda pra cima (button)
+            valor = Int32.Parse(ValorEntry.Text);
+            total = gorjeta + valor;
+
+        }
+    }
+
+}
